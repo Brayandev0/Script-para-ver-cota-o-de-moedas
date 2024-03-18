492946d781bc3261a2ee70c5ea0fd7a42bb708b3
Brayan silva <84828739+Brayandev0@users.noreply.github.com>
2024-03-18 19:04:31 -0300

    Update ver_cotacao_de_moedas.py

--- ver_cotacao_de_moedas.py
+++ ver_cotacao_de_moedas.py
@@ -3,17 +3,27 @@
 # versão          : 1.0
 # data da criação : 19/2/2024
 #
+import platform 
+import os
 import requests
 #--------------------------------------------------------------------------------------
 #                                   Variaveis padrões
-barras = 15 * "-"
+def limpador():
+    sistema_operacional = platform.system()
+    if sistema_operacional == "Windows":
+        limpador = "cls"
+    elif sistema_operacional == "Linux" or sistema_operacional == "Mac":
+        limpador = "clear"
+    return os.system(limpador)
 sucesso = 201,202,200
 MENU = " \n Insira qual moeda você quer ver a cotação \n \n Moedas disponiveis : \n \n [D] Dolar   | [ER] Euro \n \n [B] Bitcoin | [ET] ethereum  \n\n [All] todas | [BOL] bolivar venezuelano \n\n \n insira : "
 #--------------------------------------------------------------------------------------
 #                                 Definindo a moeda escolhida 
+limpador()
 tipo_de_moeda = input(MENU).lower()
 #--------------------------------------------------------------------------------------
 #                               Definindo o menu de escolha 
+limpador()
 match tipo_de_moeda:
     case "d":
         moeda_requisitada = "USD-BRL"
@@ -45,13 +55,13 @@ if codigo_status == 200 or codigo_status == 201:
 #--------------------------------------------------------------------------------------
 #                                          Realizando um for pelos dados recebidos
     for indice , valor_moeda in dados.items():
-            print(barras)
             print(f"\n codigo da moeda :{ valor_moeda["code"]}")
             print(f"\n nome da moeda : {valor_moeda["name"]}")
+            print(f"\n Preço de compra : {valor_moeda["bid"]} R$")
             print(f"\n Alta : 1 {valor_moeda["code"]} vale  {valor_moeda["high"]} R$")
             print(f"\n baixa : 1 {valor_moeda["code"]} vale {valor_moeda["low"]} R$")
             print(f"\n Data e horario da consulta : {valor_moeda["create_date"]} \n")
-            print(barras)
+
 #--------------------------------------------------------------------------------------
 #                                   Erro de comunicação 
 else: 
